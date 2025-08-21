96a718c7d751e115993243fbb98e2cf1962dfcf9
Shahab Cypher <shahabcypher@gmail.com>
2025-08-21 18:41:20 +0330

    added connect to database

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import connectDB from "./config/connectDB.js";
+
 const PORT = process.env.PORT;
 
 const startServer = async () => {
+  await connectDB();
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
